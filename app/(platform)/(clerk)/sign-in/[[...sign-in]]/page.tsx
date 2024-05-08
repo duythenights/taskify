1d28658aad236bfa18294100afdfb15dba834907
Richard_DinhHoangDuy <dinhoangduy@gmail.com>
2024-05-08 23:37:08 +0700

    fix: afterSignInUrl has duplicated

--- app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
+++ app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function SignInPage() {
   return (
-    <SignIn/>
+    <SignIn fallbackRedirectUrl={"/"}/>
   )
 }
--- app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
+++ app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function SignUpPage() {
   return (
-    <SignUp/>
+    <SignUp fallbackRedirectUrl={"/"}/>
   )
 }
